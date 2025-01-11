b0090e2740b1ba5b723b0bd767dc41d096adc884
amir <mirjalili16@gmail.com>
2025-01-11 16:23:42 +0330

    feat: add cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 const render = require("./controllers/mermaid");
 const uploadExcel = require("./controllers/uploadExcel");
 const uploadMiddleware = require("./middleware/upload");
