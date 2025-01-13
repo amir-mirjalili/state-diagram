c5131a5c7102f7a70837ac29ad4a2892a707acfa
amir <mirjalili16@gmail.com>
2025-01-13 10:48:49 +0330

    feat: remove ..

--- services/excel/convertExcelToJSON.js
+++ services/excel/convertExcelToJSON.js
@@ -53,12 +53,12 @@ skinparam {
       if (item.parentId !== 5 || item.parentId !== 6 || item.parentId !== 7) {
         diagram += `class ${item.id} < <b>  ${item.desc || ""}   > {\n`;
 
-        diagram += `..<b><size:18>${(item.chartUnit || "")
+        diagram += `<b><size:18>${(item.chartUnit || "")
           .replace(/-(.+?)(\s|$)/g, `<font:Arial>$1</font>`)
           .replace(
             /^(.*)<font:Arial>(.*?)<\/font>(.*)$/g,
             `<font:Arial>$2 </font> $1$3`
-          )}..\n`;
+          )}\n`;
       }
     } else {
       diagram += `class ${item.id} < <b>  ${item.desc || ""}   > {\n`;
